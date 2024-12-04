c0f8136b184bf3104e139d4d4ec304a21c3b8240
Bruno Castro <bcastrop1990@gmail.com>
2024-12-04 16:13:02 -0500

    se agrego primer push

---
+++ src/main/java/com/reto/tecnico/bcastro/BcastroApplication.java
@@ -0,0 +1,13 @@
+package com.reto.tecnico.bcastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BcastroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reto/tecnico/bcastro/BcastroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reto.tecnico.bcastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
